b93a7543beddb2f224017d75d64ea756f39b5223
Andrew Miyaguchi <Andrew Miyaguchi>
2020-01-22 19:35:09 -0800

    Include mkspiffs

--- SmartEgg_Firmware/FlashSpiffs.bat
+++ SmartEgg_Firmware/FlashSpiffs.bat
@@ -1,2 +1,2 @@
-mkspiffs -c data -b 4096 -p 256 -s 0x233000 spiffs.bin
-esptool --chip esp32 --port COM6 --baud 921600 write_flash -z 0x1CD000 spiffs.bin
+tools\mkspiffs.exe -c data -b 4096 -p 256 -s 0x233000 spiffs.bin
+esptool.py --chip esp32 --port COM6 --baud 921600 write_flash -z 0x1CD000 spiffs.bin

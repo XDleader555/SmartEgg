30b244def9d1d112a5cc0724cd5197fa76a0b607
Andrew Miyaguchi <XDleader555@gmail.com>
2020-01-24 04:23:04 -0800

    Make sure to use the built in esp-idf mkspiffs or nothing will ever work

--- SmartEgg_Firmware/FlashSpiffs.bat
+++ SmartEgg_Firmware/FlashSpiffs.bat
@@ -2,8 +2,8 @@
 IF [%1]==[-p] (IF [%2] == [] GOTO :err ELSE GOTO :flash) ELSE GOTO :err
 
 :flash
-tools\mkspiffs.exe -c data -b 4096 -p 256 -s 0x233000 spiffs.bin
-esptool.py --chip esp32 --port %2 --baud 921600 write_flash -z 0x1CD000 spiffs.bin
+mkspiffs -c data -b 4096 -p 256 -s 0x233000 spiffs.bin
+esptool --chip esp32 --port %2 --baud 921600 write_flash -z 0x1CD000 spiffs.bin
 exit /b
 
 :err
